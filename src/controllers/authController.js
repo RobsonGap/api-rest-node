c1971e0baa7182fbaefc0b64f57b41dd7818b936
RobsonGap <robsonmie@hotmail.com>
2020-08-15 18:44:43 -0300

    Criando uma nova rota
    Criando uma nova rota de autenticação e verificação e validação usando o bcrypt compare e verificar se as senhas não batem. as senhas não batendo ele retorna um erro.

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,7 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 
-const User = require('../models/user')
+const User = require('../models/user');
 
 const router = express.Router();
 
@@ -19,6 +20,20 @@ router.post('/register', async (req, res) => {
   } catch (err) {
     return res.status(400).send({ error: 'Registration failed' });
   }
+});
+
+router.post('/authenticate', async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user) 
+    return res.status(400).send({ erro: 'User not found' });
+
+  if (!await bcrypt.compare(password, user.password))
+      return res.status(400).send({ error: 'Invalid password' });
+
+      res.send({ user });
 });
 
 module.exports = app => app.use('/auth', router);
