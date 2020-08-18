cbbc9b44c8b1bbf63526c6c323d47f4fb568feef
RobsonGap <robsonmie@hotmail.com>
2020-08-18 06:06:29 -0300

    Adicionando dois modulos ao package.json
    Adicionando dois modulos ao package.json, o modulo fs permite trabalhar com o file system do node.js
    já o path, e para  trabalhar com caminhos de pastas.!
    não preciso mais importar todos controllers todos criados ágora são importados automaticamente.

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@
  app.use(bodyParser.json());
  app.use(bodyParser.urlencoded({ extended: false }));
 
-require('./app/controllers/authController')(app);
-require('./app/controllers/projectController')(app);
+require('./controllers/index')(app);
+
 
 
  app.listen(3000);
