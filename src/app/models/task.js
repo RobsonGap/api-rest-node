3cdd28a07f95f8f9cb17685a32525063cf692ee1
RobsonGap <robsonmie@hotmail.com>
2020-08-24 15:42:25 -0300

    Definindo os models Task
    Project e Task um usuário vai poder criar um project e cada project vai poder ter varios tasks criando os models de Project e Task, com isso consigo atribuir tarefas aos usuários.

---
+++ src/app/models/task.js
@@ -0,0 +1,28 @@
+const mongoose = require('../../database');
+const bcrypt = require('bcryptjs');
+
+const TaskSchema = new mongoose.Schema({
+title: {
+    type: String,
+    require: true,
+},
+project: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Project',
+    require: true,
+},
+assignedTo: {
+ type: mongoose.Schema.Types.ObjectId,
+ ref: 'User',
+ require: true,
+},
+createdAt: {
+    type: Date,
+    default: Date.now,
+},
+});
+
+const Task = mongoose.model('Task', TaskSchema);
+
+module.exports = Task;
+
