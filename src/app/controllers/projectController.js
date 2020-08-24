8c0faf55b500828158346a42f3fb5db73caa2b8b
RobsonGap <robsonmie@hotmail.com>
2020-08-24 16:22:42 -0300

    Update projectController.js
    Importando e criando rotas dos novos models Task e Project

--- src/app/controllers/projectController.js
+++ src/app/controllers/projectController.js
@@ -1,15 +1,33 @@
 const express = require('express');
 const authMiddleware = require('../middlewares/auth');
 
+const Project = require('../models/project');
+const Task = require('../models/task');
+
 const router = express.Router();
 
 router.use(authMiddleware);
 
 
 
-router.get('/', (req, res) => {
-    res.send({ ok: true, user: req.userId });
+router.get('/', async (req, res) => {
+    res.send({  user: req.userId });
 });
 
+router.get('/:projectId', async (req, res) => {
+    res.send({  user: req.userId });
+});
+
+router.post('/', async (req, res) => {
+    res.send({ user: req.userId });
+});
+
+router.put('/:projectId', async (req, res) => {
+    res.send({  user: req.userId });
+});
+
+router.delete('/:projectId', async (req, res) => {
+    res.send({  user: req.userId });
+});
 
 module.exports = app => app.use('/projects', router);
