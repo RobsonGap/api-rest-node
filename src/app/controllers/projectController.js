717beedd552f3a1e3a4aa7abd81ef8b8a264e745
RobsonGap <robsonmie@hotmail.com>
2020-08-27 09:56:31 -0300

    Criando Rota de Listagem
    Criando rota de listagem, essa nova rota vai listar o projetos existentes.

--- src/app/controllers/projectController.js
+++ src/app/controllers/projectController.js
@@ -11,7 +11,13 @@ router.use(authMiddleware);
 
 
 router.get('/', async (req, res) => {
-    res.send({  user: req.userId });
+    try {
+      const projects = await Project.find();
+
+      return res.send({ projects });
+    } catch (err) {
+        return res.status(400).send({ error: 'Error loading projects' });
+    }
 });
 
 router.get('/:projectId', async (req, res) => {
