b859391e0ab63831aa3ac3c9add10089023a3d1c
RobsonGap <robsonmie@hotmail.com>
2020-08-28 15:35:14 -0300

    criando o update
    atualizando os projetos e criando novas tasks quando criar um projeto novo.
    finalizando esse projeto por equanto.!

--- src/app/controllers/projectController.js
+++ src/app/controllers/projectController.js
@@ -51,7 +51,31 @@ router.get('/:projectId', async (req, res) => {
  } );
 
 router.put('/:projectId', async (req, res) => {
-    res.send({  user: req.userId });
+    try {
+        const { title, description, tasks } = req.body;
+  
+     const project = await Project.findByIdAndUpdate(req.params.projectId, {
+        title,
+        description,
+    }, { new: true });
+
+    project.tasks = [];
+    await Task.remove({ project: project._id });
+  
+     await Promise.all(tasks.map (async task => {
+         const projectTask = new Task({ ...task, project: project._id });       
+  
+       await  projectTask.save();
+       
+             project.tasks.push(projectTask);
+         }));
+     
+     await project.save();
+  
+     return res.send({ project });
+       } catch (err) {  console.log(err)
+           return res.status(400).send({ error: 'Error updating  project' });
+       }
 });
 
 router.delete('/:projectId', async (req, res) => {
