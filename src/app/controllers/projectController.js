900559bc32236ed2d6b70fae2660ea2aea9bba3d
RobsonGap <robsonmie@hotmail.com>
2020-08-28 14:18:25 -0300

    Deixando asincrono as tasks
    Deixando asincrono as tasks usando await.

--- src/app/controllers/projectController.js
+++ src/app/controllers/projectController.js
@@ -8,11 +8,9 @@ const router = express.Router();
 
 router.use(authMiddleware);
 
-
-
 router.get('/', async (req, res) => {
     try {
-      const projects = await Project.find().populate('user');
+      const projects = await Project.find().populate(['user','tasks']);
 
       return res.send({ projects });
     } catch (err) {
@@ -22,7 +20,7 @@ router.get('/', async (req, res) => {
 
 router.get('/:projectId', async (req, res) => {
     try {
-        const project = await Project.findById(req.params.projectId).populate('user');
+        const project = await Project.findById(req.params.projectId).populate(['user','tasks']);
   
         return res.send({ project });
       } catch (err) {
@@ -30,26 +28,27 @@ router.get('/:projectId', async (req, res) => {
       }
 });
 
-router.post('/', async (req, res) => {
-   try {
-    const { title, description, tasks } = req.body;
-    
-    const project = await Project.create({ title, description, user: req.userId }); 
+ router.post('/', async (req, res) => {
+     try {
+      const { title, description, tasks } = req.body;
 
-    tasks.map(task => {
-        const projectTask = new Task({ ...task, project: project._id })
-    
-       projectTask.save().then(task =>  project.tasks.push(task));
-    });
+   const project = await Project.create({title, description, user: req.userId  });
 
-    await project.save();
+   await Promise.all(tasks.map (async task => {
+       const projectTask = new Task({ ...task, project: project._id });       
 
+     await  projectTask.save();
+     
+           project.tasks.push(projectTask);
+       }));
+   
+   await project.save();
 
-    return res.send({ project });
-   } catch (err) {
-    return res.status(400).send({ error: 'Error creating new project' });
-    }
-});
+   return res.send({ project });
+     } catch (err) {  console.log(err)
+         return res.status(400).send({ error: 'Error creating new project' });
+     }
+ } );
 
 router.put('/:projectId', async (req, res) => {
     res.send({  user: req.userId });
